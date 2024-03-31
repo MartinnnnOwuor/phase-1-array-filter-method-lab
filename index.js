54befed00fd365691c886f9680ba9e5ebf2d2f01
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-03-31 20:33:52 +0300

    update  .filter() assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  }
+  
+  const driverNames = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+  const namesThatMatch = findMatching(driverNames, 'bobby');
+  console.log(namesThatMatch);
+
+
+function fuzzyMatch(drivers, strings) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(strings.toLowerCase()));
+  }
+  
+  
+  const driverName = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+  const groupedDrivers = fuzzyMatch(driverName, 'Sa');
+  console.log(matchedDrivers); 
+
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
+  
+  const driversObjects = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+  ];
+  
+  const matchedDrivers = matchName(driversObjects, 'Bobby');
+  console.log(groupedDrivers);
